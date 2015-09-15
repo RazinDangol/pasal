19584a8267e73588066eaef9929552f3c7e15be5
Razin <dangolrajin@gmail.com>
2015-09-15 09:34:55 +0545

    Added templates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render , render_to_response
 
 # Create your views here.
+from .models import Product
+
+def show(request):
+    products=Product.objects.all()
+    print(products)
+    return render_to_response('show.html',{'products':products})
