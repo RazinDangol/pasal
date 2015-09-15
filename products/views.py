e446b099f2e0be38f3fc87df2165cf772bc66b5c
Razin Dangol <dangolrajin@gmail.com>
2015-09-15 10:13:55 +0545

    added product types to views

--- products/views.py
+++ products/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render , render_to_response
 
 # Create your views here.
-from .models import Product
+from .models import Product,ProductType
 
 def show(request):
     products=Product.objects.all()
-    print(products)
     return render_to_response('show.html',{'products':products})
